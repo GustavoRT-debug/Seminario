d1cc37f4b952d27563e2a5580ebf01e484169ffe
GusttavoTorres <88936210+GustavoRT-debug@users.noreply.github.com>
2024-05-23 20:20:44 -0300

    Update coverage_report.rb

--- coverage_report.rb
+++ coverage_report.rb
@@ -1,7 +1,1 @@
-require 'json'
 
-coverage_file = 'coverage/.last_run.json'
-coverage_data = JSON.parse(File.read(coverage_file))
-
-covered_percent = coverage_data['result']['covered_percent']
-puts "Cobertura de Testes: #{covered_percent.round(2)}%"
