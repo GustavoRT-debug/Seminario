d5d80ed6107a0cc331064c9805e4ed90ab911ab1
GustavoRT-debug <gustavo_ramos@gec.inatel.br>
2024-05-23 20:19:12 -0300

    Corrigindo path da pasta de testes para execução

--- calculadora/calculadora.rb
+++ calculadora/calculadora.rb
@@ -1,55 +1,39 @@
 class Calculator
-    def initialize
-      @running = true
-    end
-  
-    def start
-      puts "Bem-vindo à Calculadora em Ruby!"
-      while @running
-        puts "Escolha uma operação: +, -, *, / ou digite 'sair' para terminar"
-        operation = gets.chomp
-  
-        break if operation.downcase == 'sair'
-  
-        if %w[+ - * /].include?(operation)
-          puts "Digite o primeiro número:"
-          num1 = gets.chomp.to_f
-  
-          puts "Digite o segundo número:"
-          num2 = gets.chomp.to_f
-  
-          result = calculate(num1, num2, operation)
-          puts "O resultado de #{num1} #{operation} #{num2} é: #{result}"
-        else
-          puts "Operação inválida. Por favor, tente novamente."
-        end
-      end
-      puts "Calculadora finalizada."
-    end
-  
-    private
-  
-    def calculate(num1, num2, operation)
-      case operation
-      when '+'
-        num1 + num2
-      when '-'
-        num1 - num2
-      when '*'
-        num1 * num2
-      when '/'
-        if num2 != 0
-          num1 / num2
-        else
-          "Erro: Divisão por zero!"
-        end
-      else
-        "Operação inválida."
-      end
+  def calculate(a, b, operation)
+    case operation
+    when '+'
+      a + b
+    when '-'
+      a - b
+    when '*'
+      a * b
+    when '/'
+      return "Erro: Divisão por zero!" if b == 0
+      a / b
+    else
+      "Operação inválida."
     end
   end
-  
-  # Cria uma nova instância da calculadora e inicia
-  calculator = Calculator.new
-  calculator.start
-  
+
+  def self.start
+    puts "Bem-vindo à Calculadora em Ruby!"
+    loop do
+      puts "Escolha uma operação: +, -, *, / ou digite 'sair' para terminar"
+      operation = gets.chomp
+
+      break if operation == 'sair'
+
+      puts "Digite o primeiro número:"
+      a = gets.chomp.to_i
+
+      puts "Digite o segundo número:"
+      b = gets.chomp.to_i
+
+      result = Calculator.new.calculate(a, b, operation)
+      puts "Resultado: #{result}"
+    end
+  end
+end
+
+# Descomente a linha abaixo para executar o programa normalmente
+Calculator.start if __FILE__ == $0
