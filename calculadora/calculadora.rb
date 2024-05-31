a4326ba41b7dd76c89755c8d35dbddce3e753496
MarceloAbrantes <marcelo.abrantes@gec.inatel.br>
2024-05-30 21:47:58 -0300

    Mudanças na calculadora

--- calculadora/calculadora.rb
+++ calculadora/calculadora.rb
@@ -1,5 +1,6 @@
 class Calculator
   def calculate(a, b, operation)
+    # Vê qual foi a operação matemática especificada
     case operation
     when '+'
       a + b
@@ -10,15 +11,35 @@ class Calculator
     when '/'
       return "Erro: Divisão por zero!" if b == 0
       a / b
+    when 'raiz'
+      Math.sqrt(a)
+    when '%'
+      a % b
+    when '**'
+      a ** b
+    when '!'
+      factorial(a)    
+    # Se a operação não for válida, retorna uma mensagem de erro  
     else
       "Operação inválida."
+    end
+  end
+  
+  #Função para o calculo do fatorial
+  def factorial(n)
+    if n == 0
+      1
+    else
+      (1..n).inject(:*)
     end
   end
 
   def self.start
     puts "Bem-vindo à Calculadora em Ruby!"
+
+    #Inicia um looping até o usuário escolher sair sair
     loop do
-      puts "Escolha uma operação: +, -, *, / ou digite 'sair' para terminar"
+      puts "Escolha uma operação: +, -, *, /, raiz, %, **, ! ou digite 'sair' para terminar"
       operation = gets.chomp
 
       break if operation == 'sair'
@@ -26,9 +47,15 @@ class Calculator
       puts "Digite o primeiro número:"
       a = gets.chomp.to_i
 
-      puts "Digite o segundo número:"
-      b = gets.chomp.to_i
-
+      # se a operação for raiz ou fatorial apenas o "a" é necessário
+      if operation != 'raiz' && operation != '!'
+        puts "Digite o segundo número:"
+        b = gets.chomp.to_i
+      else
+        b = nil
+      end
+      
+    # Chama o método calculate para realizar a operação
       result = Calculator.new.calculate(a, b, operation)
       puts "Resultado: #{result}"
     end
