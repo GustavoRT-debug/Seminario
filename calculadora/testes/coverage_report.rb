64412fb01656c405aee3e9d1e47f9e97973c93af
MarceloAbrantes <99184020+MarceloAbrantes@users.noreply.github.com>
2024-06-02 23:48:27 -0300

    Deletar o coverage_report.rb para rodar o ci/cd...

--- calculadora/testes/coverage_report.rb
+++
@@ -1,9 +0,0 @@
-# coverage_report.rb
-
-require 'simplecov'
-SimpleCov.start
-
-# Carregue e execute os testes aqui
-require_relative 'testes.rb'
-require_relative 'teste2.rb'
-require_relative 'teste3_mock.rb'
