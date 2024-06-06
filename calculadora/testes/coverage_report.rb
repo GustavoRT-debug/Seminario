d80357ecf038c959438a15b8eb525e7c3783c487
MarceloAbrantes <99184020+MarceloAbrantes@users.noreply.github.com>
2024-06-05 21:55:03 -0300

    Update coverage_report.rb

--- calculadora/testes/coverage_report.rb
+++ calculadora/testes/coverage_report.rb
@@ -3,6 +3,5 @@ SimpleCov.start do
   coverage_dir 'coverage'  # Configura a saída do relatório HTML para a pasta `coverage`
 end
 # Carregue e execute os testes aqui
-require_relative 'testes.rb'
 require_relative 'teste2.rb'
 require_relative 'teste3_mock.rb'
